4dc3511bd3670d189d825b6c2c87f22f24685c86
Trish Whetzel <plwhetzel@gmail.com>
2014-07-06 20:15:00 -0700

    add method to add class restrictions, remove incorrect method using OWLIndividual to add ObjectProperty

--- Tab2OWL/src/ConvertFile.java
+++ Tab2OWL/src/ConvertFile.java
@@ -161,9 +161,9 @@ public class ConvertFile {
 	 */
 	private static void buildClassTree(Map<String, ArrayList> termsAndProperties,
 			File owlFile) throws OWLOntologyCreationException, OWLOntologyStorageException {
-		
+
 		// Has Role -> http://purl.obolibrary.org/obo/BFO_0000087 (object property)
-		
+
 		// Open ontology file
 		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
 		OWLOntology ontology = manager.loadOntologyFromOntologyDocument(owlFile);
@@ -188,70 +188,69 @@ public class ConvertFile {
 			OWLClass clsB = factory.getOWLClass(IRI.create(prefix + newParent));
 			OWLAxiom axiom = factory.getOWLSubClassOfAxiom(clsAMethodA, clsB);
 			AddAxiom addAxiom = new AddAxiom(ontology, axiom);
-	        // We now use the manager to apply the change
-	        manager.applyChange(addAxiom);
+			// We now use the manager to apply the change
+			manager.applyChange(addAxiom);
 
+			
+			// Try to add Has Role (object property) as a restriction on a Class
+			String IAO = "http://purl.obolibrary.org/obo/";  
+			// Create Object Property
+			OWLObjectProperty hasRoleProperty = factory.getOWLObjectProperty(IRI.create(IAO
+					+ "BFO_0000087"));
+			System.err.println("hasRoleProperty: "+hasRoleProperty);
+
+	        //OWLClassExpression hasPartSomeRole = factory.getOWLObjectSomeValuesFrom(hasRoleProperty,
+	        //		clsAMethodA); 
 	        
-	        // Try to add Has Role (object property) as a Class restriction as a S-P-O triple
-	        // NOTE: Object properties connect pairs of individuals. 
-	        String IAO = "http://purl.obolibrary.org/obo/";
-	        // Has Role -> http://purl.obolibrary.org/obo/BFO_0000087 (object property)
-	        // Create Object Property
-	        OWLObjectProperty hasRoleProperty = factory.getOWLObjectProperty(IRI.create(IAO
-	                + "BFO_0000087"));
-	        //System.err.println("hasRoleProperty: "+hasRoleProperty);
-	        
-	        // Get OWLIndividual for Subject
-	        OWLIndividual clsAMethodASubject = factory.getOWLNamedIndividual(IRI.create(prefix+newKey));
-	        System.err.println("SUBJECT: "+clsAMethodASubject);
-	 
-	        // Get Class object for Object, values[3]
+	        // Obtain a reference to the Head class so that we can specify that Heads have Noses
+	        // Get Class object for HasRole, values[3]
 	        String hasRoleObject = entry.getValue().get(3).toString();
-	        //System.err.println("HasRoleObject: "+hasRoleObject);
+	        System.err.println("HasRoleObject: "+hasRoleObject);
 	        String newHasRoleObject = hasRoleObject.replace(":Category:","");
 	        newHasRoleObject = newHasRoleObject.replaceAll("\"", "");
-	        //System.err.println("HasRoleObject-MOD: "+newHasRoleObject);
-	        
-	        
-	        // Add property to class if hasRoleObject exists
-	        if (!hasRoleObject.equals("NO VALUE")) {
-	        	//System.err.println("VALUE FOUND: "+newHasRoleObject);
-	        	//NOTE: newHasRoleObject may have more than 1 object value
-	        	String[] hasRoleObjectValues = newHasRoleObject.split(",");
-	        	// Add each hasRoleObect value as a property restriction
-	        	for (String s : hasRoleObjectValues ) {
-	        		s = s.replaceAll(" ", "_");
-	        		System.err.println("hasRoleObjectValues: "+s);
-	        		//OWLClass hasRoleObjectClass = factory.getOWLClass(IRI.create(prefix + s));
-	        		OWLIndividual hasRoleObjectClass = factory.getOWLNamedIndividual(IRI.create(prefix
-	                        + s));
-	        		System.err.println("OBJECT: "+hasRoleObjectClass);
-	        		// Now create the actual assertion (triple), as an object property
-	                // assertion axiom matthew --> hasFather --> peter
-	        		OWLObjectPropertyAssertionAxiom assertion = factory
-	        				.getOWLObjectPropertyAssertionAxiom(hasRoleProperty, clsAMethodASubject, hasRoleObjectClass);
-	                
-	        		// TODO Finally, add the axiom to our ontology and save
-	                AddAxiom addAxiomChange = new AddAxiom(ontology, assertion);
-	                manager.applyChange(addAxiomChange);
-	        	}
+	        System.err.println("HasRoleObject-MOD: "+newHasRoleObject);
+
+	        //OWLClass head = factory.getOWLClass(IRI.create(prefix + newHasRoleObject));
+	        // We now want to state that Head is a subclass of hasPart some Nose, to
+	        // do this we create a subclass axiom, with head as the subclass and
+	        // "hasPart some Nose" as the superclass (remember, restrictions are
+	        // also classes - they describe classes of individuals -- they are
+	        // anonymous classes).
+	        if (!newHasRoleObject.equals("NO VALUE")) {
+				System.err.println("VALUE FOUND: "+newHasRoleObject);
+				//NOTE: newHasRoleObject may have more than 1 object value
+				String[] hasRoleObjectValues = newHasRoleObject.split(",");
+				// Add each hasRoleObect value as a property restriction
+				for (String s : hasRoleObjectValues ) {
+					s = s.replaceAll(" ", "_");
+					System.err.println("hasRoleObjectValues: "+s);
+					
+					OWLClass roleObject = factory.getOWLClass(IRI.create(prefix + s));
+					//OWLSubClassOfAxiom ax = factory.getOWLSubClassOfAxiom(roleObject, hasPartSomeRole); //statement is backwards 
+					
+					OWLClassExpression hasPartSomeRole = factory.getOWLObjectSomeValuesFrom(hasRoleProperty,
+							roleObject); 
+					OWLSubClassOfAxiom ax = factory.getOWLSubClassOfAxiom(clsAMethodA, hasPartSomeRole);	
+					System.out.println("RoleObject: "+roleObject+"\nAxiom: "+ax);
+					
+					// Add the axiom to our ontology 
+					AddAxiom addAx = new AddAxiom(ontology, ax);
+					manager.applyChange(addAx);
+				}
 	        }
-	       
-	        
-	        
 
 			// We can add a declaration axiom to the ontology, that essentially adds
 			// the class to the signature of our ontology. 
 			OWLDeclarationAxiom declarationAxiom = factory
 					.getOWLDeclarationAxiom(clsAMethodA);
 			manager.addAxiom(ontology, declarationAxiom);
-		
+
 			// Save ontology 
 			manager.saveOntology(ontology);	
 		}		
 	}
 
-	
+
 	/**
 	 * 
 	 * @param termsAndProperties
@@ -259,19 +258,19 @@ public class ConvertFile {
 	 */
 	private static void addClassRestrictions(
 			Map<String, ArrayList> termsAndProperties, File owlFile) {
-		   
+
 		// Add restrictions to the Class 
 		// Declare property IRIs
 		//String IAO = "http://purl.obolibrary.org/obo/";
-        // TODO Create and Add these properties to the ontology
-        // Term Label -> rdfs:label
-        // Definition -> http://purl.obolibrary.org/obo/IAO_0000115
-        // Has Role -> http://purl.obolibrary.org/obo/BFO_0000087 (object property)
+		// TODO Create and Add these properties to the ontology
+		// Term Label -> rdfs:label
+		// Definition -> http://purl.obolibrary.org/obo/IAO_0000115
+		// Has Role -> http://purl.obolibrary.org/obo/BFO_0000087 (object property)
 		// Is Part Of -> http://purl.obolibrary.org/obo/BFO_0000050 (object property, transitive Has Part), see Brain in Uberon for example usage 
-        // Has Part -> http://purl.obolibrary.org/obo/BFO_0000051 (object property, transitive Is Part Of)
-        // Synonym ->  http://purl.obolibrary.org/obo/IAO_0000118 
-        // Defining Citation (similar to xref) ->  http://purl.obolibrary.org/obo/IAO_0000301      
-        
+		// Has Part -> http://purl.obolibrary.org/obo/BFO_0000051 (object property, transitive Is Part Of)
+		// Synonym ->  http://purl.obolibrary.org/obo/IAO_0000118 
+		// Defining Citation (similar to xref) ->  http://purl.obolibrary.org/obo/IAO_0000301      
+
 		/*OWLObjectProperty hasPart = factory.getOWLObjectProperty(IRI.create(IAO
                 + "#hasPart"));
         OWLClass nose = factory.getOWLClass(IRI.create(prefix + clsAMethodA));
@@ -291,9 +290,9 @@ public class ConvertFile {
         // Add the axiom to our ontology
         AddAxiom addAx = new AddAxiom(ont, ax);
         man.applyChange(addAx);*/
-        
- 	
+
+
 	}
-	
+
 
 }

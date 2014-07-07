79d65cfb2117b12a15b5f4dedcbb6f56939647f2
Trish Whetzel <plwhetzel@gmail.com>
2014-07-07 13:30:56 -0700

    add to method addAnnotations to account for additional annotations in text file

--- Tab2OWL/src/ConvertFile.java
+++ Tab2OWL/src/ConvertFile.java
@@ -14,6 +14,8 @@ import org.semanticweb.owlapi.io.StreamDocumentTarget;
 import org.semanticweb.owlapi.model.AddAxiom;
 import org.semanticweb.owlapi.model.AddImport;
 import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLAnnotationProperty;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLClassExpression;
@@ -29,7 +31,9 @@ import org.semanticweb.owlapi.model.OWLOntologyID;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+import org.semanticweb.owlapi.model.PrefixManager;
 import org.semanticweb.owlapi.model.SetOntologyID;
+import org.semanticweb.owlapi.util.DefaultPrefixManager;
 import org.semanticweb.owlapi.util.SimpleIRIMapper;
 
 
@@ -49,16 +53,21 @@ public class ConvertFile {
 		 */
 		Map<String, ArrayList> termsAndProperties = processTermFile();	
 		File owlFile = createOWLFile();
+		System.out.println("** Returned OWLFile: "+owlFile.getAbsolutePath());
 		buildClassTree(termsAndProperties, owlFile);
-		// Add method to add properties to owl File???
-		//addClassRestrictions(termsAndProperties, owlFile);
+		
+		addClassRestrictions(termsAndProperties, owlFile);
+		addAnnotations(termsAndProperties, owlFile);
 	}
+
+
 
 	/**
 	 * Read in file and generate data structure of terms and their properties 
 	 * @return
 	 */
 	private static Map<String, ArrayList> processTermFile() {
+		System.out.println("** processTermFile method **");
 		BufferedReader br = null;
 		Map<String, ArrayList> terms = new HashMap<String, ArrayList>();
 		ArrayList<String> list = new ArrayList<String>();
@@ -130,16 +139,19 @@ public class ConvertFile {
 		// Specify IRIs for ontology imports
 		IRI bfoIRI = IRI.create("http://purl.obolibrary.org/obo/bfo.owl");
 		IRI iaoIRI = IRI.create("http://purl.obolibrary.org/obo/iao.owl");
-			
+
 		try {
 			ontology = manager.createOntology(ontologyIRI);
 			OWLDataFactory factory = manager.getOWLDataFactory();
 			// Create the document IRI for our ontology
-			IRI documentIRI = IRI.create("/Users/whetzel/Documents/workspace/Tab2OWL/");
+			//IRI documentIRI = IRI.create("/Users/whetzel/Documents/workspace/Tab2OWL/");
+			IRI documentIRI = IRI.create("/Users/whetzel/git/tab2owl/Tab2OWL/");  //Local Git repo
+			
 			// Set up a mapping, which maps the ontology to the document IRI
 			SimpleIRIMapper mapper = new SimpleIRIMapper(ontologyIRI, documentIRI);
 			manager.addIRIMapper(mapper);
 			System.out.println("Created ontology: " + ontology);
+			
 			// Set version IRI, use the date the file contents were exported from NeuroLex
 			IRI versionIRI = IRI.create(ontologyIRI + "NeuroLexExport06302014");
 			OWLOntologyID newOntologyID = new OWLOntologyID(ontologyIRI, versionIRI);
@@ -149,17 +161,17 @@ public class ConvertFile {
 			manager.applyChange(setOntologyID);
 			System.out.println("Ontology: " + ontology);
 
-			
+
 			// Need to add imports for BFO and IAO to ontology
-			OWLImportsDeclaration bfoImportDeclaraton =
-			   factory.getOWLImportsDeclaration(bfoIRI); 
+			/*OWLImportsDeclaration bfoImportDeclaraton =
+					factory.getOWLImportsDeclaration(bfoIRI); 
 			manager.applyChange(new AddImport(ontology, bfoImportDeclaraton));
-			
+
 			OWLImportsDeclaration iaoImportDeclaraton =
-					   factory.getOWLImportsDeclaration(iaoIRI); 
-					manager.applyChange(new AddImport(ontology, iaoImportDeclaraton));
-			
-			
+					factory.getOWLImportsDeclaration(iaoIRI); 
+			manager.applyChange(new AddImport(ontology, iaoImportDeclaraton));
+*/
+
 			// Now save a local copy of the ontology. (
 			manager.saveOntology(ontology, IRI.create(file.toURI()));
 
@@ -178,19 +190,25 @@ public class ConvertFile {
 	 */
 	private static void buildClassTree(Map<String, ArrayList> termsAndProperties,
 			File owlFile) throws OWLOntologyCreationException, OWLOntologyStorageException {
-
-		// Has Role -> http://purl.obolibrary.org/obo/BFO_0000087 (object property)
-
+		System.out.println("** \nbuildClassTree method **");
+		System.out.println("** Passed to Method OWLFILE: "+owlFile.getAbsolutePath());
+		
 		// Open ontology file
 		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		System.out.println("Created Manager: "+manager);
+		
 		OWLOntology ontology = manager.loadOntologyFromOntologyDocument(owlFile);
+		//OWLOntology ontology = manager.makeLoadImportRequest(arg0, arg1);
+		System.out.println("Loaded ontology: "+ontology);
+		
 		OWLDataFactory factory = manager.getOWLDataFactory();
-		System.out.println("Loaded ontology: " + ontology);
+		System.out.println("Created factory: " + factory);
 
+		
 		// Populate ontology with class hierarchy from termsAndProperties 
 		for (Entry<String, ArrayList> entry : termsAndProperties.entrySet()) {
 			String key = entry.getKey();	
-			System.out.println("K:"+key+"\tV:"+entry.getValue());
+			//System.out.println("K:"+key+"\tV:"+entry.getValue());
 
 			String prefix = "http://neurolex.org/wiki/Special:ExportRDF/Category:";
 			String newKey = key.replaceAll(" ", "_");
@@ -208,7 +226,11 @@ public class ConvertFile {
 			// We now use the manager to apply the change
 			manager.applyChange(addAxiom);
 
-			
+
+			/**
+			 * Add class restrictions and annotations
+			 */
+			/* THIS WORKS - TRYING ANOTHER OPTION 
 			// Try to add Has Role (object property) as a restriction on a Class
 			String IAO = "http://purl.obolibrary.org/obo/";  
 			// Create Object Property
@@ -216,23 +238,13 @@ public class ConvertFile {
 					+ "BFO_0000087"));
 			System.err.println("hasRoleProperty: "+hasRoleProperty);
 
-	        //OWLClassExpression hasPartSomeRole = factory.getOWLObjectSomeValuesFrom(hasRoleProperty,
-	        //		clsAMethodA); 
-	        
-	        // Obtain a reference to the Head class so that we can specify that Heads have Noses
-	        // Get Class object for HasRole, values[3]
+	        // Obtain a reference values for Has Role, values[3]
 	        String hasRoleObject = entry.getValue().get(3).toString();
 	        System.err.println("HasRoleObject: "+hasRoleObject);
 	        String newHasRoleObject = hasRoleObject.replace(":Category:","");
 	        newHasRoleObject = newHasRoleObject.replaceAll("\"", "");
 	        System.err.println("HasRoleObject-MOD: "+newHasRoleObject);
 
-	        //OWLClass head = factory.getOWLClass(IRI.create(prefix + newHasRoleObject));
-	        // We now want to state that Head is a subclass of hasPart some Nose, to
-	        // do this we create a subclass axiom, with head as the subclass and
-	        // "hasPart some Nose" as the superclass (remember, restrictions are
-	        // also classes - they describe classes of individuals -- they are
-	        // anonymous classes).
 	        if (!newHasRoleObject.equals("NO VALUE")) {
 				System.err.println("VALUE FOUND: "+newHasRoleObject);
 				//NOTE: newHasRoleObject may have more than 1 object value
@@ -241,20 +253,19 @@ public class ConvertFile {
 				for (String s : hasRoleObjectValues ) {
 					s = s.replaceAll(" ", "_");
 					System.err.println("hasRoleObjectValues: "+s);
-					
+
 					OWLClass roleObject = factory.getOWLClass(IRI.create(prefix + s));
-					//OWLSubClassOfAxiom ax = factory.getOWLSubClassOfAxiom(roleObject, hasPartSomeRole); //statement is backwards 
-					
 					OWLClassExpression hasPartSomeRole = factory.getOWLObjectSomeValuesFrom(hasRoleProperty,
 							roleObject); 
 					OWLSubClassOfAxiom ax = factory.getOWLSubClassOfAxiom(clsAMethodA, hasPartSomeRole);	
 					System.out.println("RoleObject: "+roleObject+"\nAxiom: "+ax);
-					
+
 					// Add the axiom to our ontology 
 					AddAxiom addAx = new AddAxiom(ontology, ax);
 					manager.applyChange(addAx);
 				}
 	        }
+			 */
 
 			// We can add a declaration axiom to the ontology, that essentially adds
 			// the class to the signature of our ontology. 
@@ -268,48 +279,186 @@ public class ConvertFile {
 	}
 
 
+
 	/**
 	 * 
 	 * @param termsAndProperties
 	 * @param owlFile
+	 * @throws OWLOntologyCreationException 
+	 * @throws OWLOntologyStorageException 
 	 */
 	private static void addClassRestrictions(
-			Map<String, ArrayList> termsAndProperties, File owlFile) {
-
-		// Add restrictions to the Class 
+			Map<String, ArrayList> termsAndProperties, File owlFile) throws OWLOntologyCreationException, OWLOntologyStorageException {
+		System.out.println("** addClassRestrictions method **");
+		
 		// Declare property IRIs
-		//String IAO = "http://purl.obolibrary.org/obo/";
-		// TODO Create and Add these properties to the ontology
-		// Term Label -> rdfs:label
-		// Definition -> http://purl.obolibrary.org/obo/IAO_0000115
 		// Has Role -> http://purl.obolibrary.org/obo/BFO_0000087 (object property)
 		// Is Part Of -> http://purl.obolibrary.org/obo/BFO_0000050 (object property, transitive Has Part), see Brain in Uberon for example usage 
 		// Has Part -> http://purl.obolibrary.org/obo/BFO_0000051 (object property, transitive Is Part Of)
-		// Synonym ->  http://purl.obolibrary.org/obo/IAO_0000118 
-		// Defining Citation (similar to xref) ->  http://purl.obolibrary.org/obo/IAO_0000301      
-
-		/*OWLObjectProperty hasPart = factory.getOWLObjectProperty(IRI.create(IAO
-                + "#hasPart"));
-        OWLClass nose = factory.getOWLClass(IRI.create(prefix + clsAMethodA));
-        // Now create a restriction to describe the class of individuals that
-        // have at least one part that is a kind of nose
-        OWLClassExpression hasPartSomeNose = factory.getOWLObjectSomeValuesFrom(hasPart,
-                nose);
-        // Obtain a reference to the Head class so that we can specify that
-        // Heads have noses
-        OWLClass head = factory.getOWLClass(IRI.create(IAO + "#Head"));
-        // We now want to state that Head is a subclass of hasPart some Nose, to
-        // do this we create a subclass axiom, with head as the subclass and
-        // "hasPart some Nose" as the superclass (remember, restrictions are
-        // also classes - they describe classes of individuals -- they are
-        // anonymous classes).
-        OWLSubClassOfAxiom ax = factory.getOWLSubClassOfAxiom(head, hasPartSomeNose);
-        // Add the axiom to our ontology
-        AddAxiom addAx = new AddAxiom(ont, ax);
-        man.applyChange(addAx);*/
 
 
+		// Open ontology file
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLOntology ontology = manager.loadOntologyFromOntologyDocument(owlFile);
+		OWLDataFactory factory = manager.getOWLDataFactory();
+		//System.out.println("Loaded ontology: " + ontology);
+
+		// Iterate through termsAndProperties
+		for (Entry<String, ArrayList> entry : termsAndProperties.entrySet()) {
+			String key = entry.getKey();	
+			//System.out.println("K:"+key+"\tV:"+entry.getValue());
+
+			PrefixManager pm = new DefaultPrefixManager(
+					"http://neurolex.org/wiki/Special:ExportRDF/Category:");
+			//String prefix = "http://neurolex.org/wiki/Special:ExportRDF/Category:";
+			String newKey = key.replaceAll(" ", "_");
+			OWLClass clsAMethodB = factory.getOWLClass(newKey, pm);
+			//System.err.println("classAMethodB: "+clsAMethodB);
+
+			/**
+			 * Add class restrictions and annotations
+			 */
+			// Try to add Has Role (object property) as a restriction on a Class
+			String BFO = "http://purl.obolibrary.org/obo/";  
+			// Create Object Property
+			OWLObjectProperty hasRoleProperty = factory.getOWLObjectProperty(IRI.create(BFO
+					+ "BFO_0000087"));
+			//System.err.println("hasRoleProperty: "+hasRoleProperty);
+
+			// Obtain a reference values for Has Role, values[3]
+			String hasRoleObject = entry.getValue().get(3).toString();
+			//System.err.println("HasRoleObject: "+hasRoleObject);
+			String newHasRoleObject = hasRoleObject.replace(":Category:","");
+			newHasRoleObject = newHasRoleObject.replaceAll("\"", "");
+			//System.err.println("HasRoleObject-MOD: "+newHasRoleObject);
+
+			if (!newHasRoleObject.equals("NO VALUE")) {
+				//System.err.println("VALUE FOUND: "+newHasRoleObject);
+				//NOTE: newHasRoleObject may have more than 1 object value
+				String[] hasRoleObjectValues = newHasRoleObject.split(",");
+				// Add each hasRoleObect value as a property restriction
+				for (String s : hasRoleObjectValues ) {
+					s = s.replaceAll(" ", "_");
+					//System.err.println("hasRoleObjectValues: "+s);
+
+					OWLClass roleObject = factory.getOWLClass(s, pm); 
+					OWLClassExpression hasPartSomeRole = factory.getOWLObjectSomeValuesFrom(hasRoleProperty,
+							roleObject); 
+					OWLSubClassOfAxiom ax = factory.getOWLSubClassOfAxiom(clsAMethodB, hasPartSomeRole);	
+					//System.err.println("RoleObject: "+roleObject+"\nAxiom: "+ax);
+
+					// Add the axiom to our ontology 
+					AddAxiom addAx = new AddAxiom(ontology, ax);
+					manager.applyChange(addAx);
+				}
+			}
+
+			// Save ontology 
+			manager.saveOntology(ontology);
+		}
 	}
 
 
+
+	private static void addAnnotations(
+			Map<String, ArrayList> termsAndProperties, File owlFile) throws OWLOntologyCreationException, OWLOntologyStorageException {
+		System.out.println("** addAnnotations method **");
+		
+		// Declare property IRIs
+		// Term Label -> rdfs:label
+		// Definition -> http://purl.obolibrary.org/obo/IAO_0000115
+		// Synonym ->  http://purl.obolibrary.org/obo/IAO_0000118 
+		// Defining Citation (similar to xref) ->  http://purl.obolibrary.org/obo/IAO_0000301 
+
+		// Open ontology file
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLOntology ontology = manager.loadOntologyFromOntologyDocument(owlFile);
+		OWLDataFactory factory = manager.getOWLDataFactory();
+		System.out.println("Loaded ontology: " + ontology);
+
+		// Iterate through termsAndProperties
+		for (Entry<String, ArrayList> entry : termsAndProperties.entrySet()) {
+			String key = entry.getKey();	
+			System.out.println("K:"+key+"\tV:"+entry.getValue());
+
+			PrefixManager pm = new DefaultPrefixManager(
+					"http://neurolex.org/wiki/Special:ExportRDF/Category:");
+			//String prefix = "http://neurolex.org/wiki/Special:ExportRDF/Category:";
+			String newKey = key.replaceAll(" ", "_");
+			OWLClass clsAMethodB = factory.getOWLClass(newKey, pm);
+			System.err.println("classAMethodB: "+clsAMethodB);
+
+			/**
+			 * Add annotations -> Label, Definition, Synonym, Defining Citation
+			 */
+			String IAO = "http://purl.obolibrary.org/obo/";  
+			// Create Annotation Properties
+			 OWLAnnotationProperty definitionProperty = factory.getOWLAnnotationProperty((IRI.create(IAO
+					+ "IAO_0000115")));
+			OWLAnnotationProperty synonymProperty = factory.getOWLAnnotationProperty((IRI.create(IAO
+					+ "IAO_0000118")));
+			OWLAnnotationProperty citationProperty = factory.getOWLAnnotationProperty((IRI.create(IAO
+					+ "IAO_0000301")));
+			//System.err.println("Annotation Properties (D,S,C): \n"+definitionProperty+"\n"+synonymProperty+"\n"+citationProperty);
+			
+			
+			// Get values for Label from text file, values[1]
+			String label = entry.getValue().get(1).toString();
+			if (!label.equals("NO VALUE")) {
+				System.err.println("VALUE FOUND: "+label);
+		        OWLAnnotation labelAnnotation = factory.getOWLAnnotation(factory.getRDFSLabel(),factory.getOWLLiteral(label));
+		        OWLAxiom labelAxiom = factory.getOWLAnnotationAssertionAxiom(clsAMethodB.getIRI(), labelAnnotation);
+		        manager.applyChange(new AddAxiom(ontology, labelAxiom));
+			}
+			
+			
+			// Get values for Synonym from text file, values[6] 
+			String synonym = entry.getValue().get(6).toString();
+			synonym = synonym.replaceAll("\"", "");
+			System.err.println("Synonym: "+synonym);
+			if (!synonym.equals("NO VALUE")) {
+				String[] synonymValues = synonym.split(",");
+				System.err.println("VALUE FOUND: "+synonymValues);
+				for (String syn : synonymValues ) {
+					OWLAnnotation synonymAnnotation = factory.getOWLAnnotation(synonymProperty,factory.getOWLLiteral(syn));
+					OWLAxiom synonymAxiom = factory.getOWLAnnotationAssertionAxiom(clsAMethodB.getIRI(), synonymAnnotation);
+					System.err.println("Synonym Axiom: "+synonymAxiom);
+					manager.applyChange(new AddAxiom(ontology, synonymAxiom)); 
+				}
+			}
+
+			// Get values for Defining Citation from text file, values[7] 
+			String citation = entry.getValue().get(7).toString();
+			System.err.println("Citation: "+citation);
+			if (!citation.equals("NO VALUE")) {
+				System.err.println("VALUE FOUND: "+citation);
+
+				OWLAnnotation citationAnnotation = factory.getOWLAnnotation(citationProperty,factory.getOWLLiteral(citation));
+				OWLAxiom citationAxiom = factory.getOWLAnnotationAssertionAxiom(clsAMethodB.getIRI(), citationAnnotation);
+				System.err.println("Synonym Axiom: "+citationAxiom);
+				manager.applyChange(new AddAxiom(ontology, citationAxiom)); 
+			}
+
+			// Get values for Definition from text file, values[8]
+			String definition = entry.getValue().get(8).toString();
+			System.err.println("Definition: "+definition);	
+			if (!definition.equals("NO VALUE")) {
+				System.err.println("VALUE FOUND: "+definition);
+		        OWLAnnotation definitionAnnotation = factory.getOWLAnnotation(definitionProperty,factory.getOWLLiteral(definition));
+		        OWLAxiom definitionAxiom = factory.getOWLAnnotationAssertionAxiom(clsAMethodB.getIRI(), definitionAnnotation);
+		        manager.applyChange(new AddAxiom(ontology, definitionAxiom));
+			}
+		}
+
+		// Save ontology 
+		manager.saveOntology(ontology);
+	}
+
+	
+	
 }
+
+
+
+
+
